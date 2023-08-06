ecb980ae139b06fffffca540a3946f9e15f2a090
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-08-06 23:22:21 +0200

    Init

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/react-todo", {
+  .connect("mongodb://127.0.0.1:27017/fullstack-todo-app", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
