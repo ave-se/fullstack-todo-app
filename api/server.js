7a260efc23fd48d89b1e57a458200eceef04a3cb
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-08-11 09:47:40 +0200

    server handling

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -15,12 +16,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(console.error);
 
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 // Models
 const Todo = require("./models/Todo");
 
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
-
   res.json(todos);
 });
 
@@ -41,28 +44,29 @@ app.post("/todo/new", (req, res) => {
 
 app.delete("/todo/delete/:id", async (req, res) => {
   const result = await Todo.findByIdAndDelete(req.params.id);
-
   res.json({ result });
 });
 
 app.get("/todo/complete/:id", async (req, res) => {
   const todo = await Todo.findById(req.params.id);
-
   todo.complete = !todo.complete;
-
   todo.save();
-
   res.json(todo);
 });
 
 app.put("/todo/update/:id", async (req, res) => {
   const todo = await Todo.findById(req.params.id);
-
   todo.text = req.body.text;
-
   todo.save();
-
   res.json(todo);
 });
 
-app.listen(3001);
+// Route to serve the React app (index.html) for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
