7d6fc88673c5c274b8600c6c379a4024fe965496
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-08-20 20:56:19 +0200

    bug fix

--- api/server.js
+++ api/server.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const mongodbUri =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mern-todo";
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mern-todo", {
+  .connect(mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
